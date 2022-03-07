c58879fc9b8b69d600f43ff3c68660a5001d9dd4
codewrapperz2021 <codewrapperz360@gmail.com>
2022-03-07 10:38:46 +0530

    fghjkl

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,6 @@
 import io from 'socket.io-client';
-// const sockets = io('http://server.eba-arbcesy3.us-east-1.elasticbeanstalk.com', { autoConnect: true, forceNew: true });
-const sockets = io('https://999a-103-179-8-210.ngrok.io', { autoConnect: true, forceNew: true });
+const sockets = io('http://server.eba-arbcesy3.us-east-1.elasticbeanstalk.com', { autoConnect: true, forceNew: true });
+// const sockets = io('https://999a-103-179-8-210.ngrok.io', { autoConnect: true, forceNew: true });
+// const sockets = io('http://localhost:3005', { autoConnect: true, forceNew: true });
+
 export default sockets;
